161fe54d5bf409a7e53db815413ca615d878d2f9
alunolages <alunolages@lages.sc>
2026-05-06 11:46:06 -0300

    save

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -2,8 +2,6 @@ export default {
 	"*.{ts,tsx,js,jsx,json}": "biome check --write",
 	"*.md": "prettier --write",
 	"apps/**/src/**/*.java": [
-		"turbo run lint --filter=tr1-playlist",
-		(files) =>
-			`java -jar tools/java/checkstyle-10.20.0-all.jar -c tools/java/checkstyle.xml ${files.join(" ")}`,
+		(files) => `npm run format:java ${files.join(" ")}`,
 	],
 };
